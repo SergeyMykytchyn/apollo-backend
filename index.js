085e1cf43520af4f87f5726f089b191bd1eddce2
Sergey Mykytchyn <sergeymykytchyn@gmail.com>
2022-04-03 18:10:46 +0300

    init

---
+++ index.js
@@ -0,0 +1,21 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection Successfull"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Backend server is running");
+});
